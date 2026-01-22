e868ae87950ee9d422a41d388049bcdda3f04ea8
A.ELKABANY <assermohamed337@gmail.com>
2026-01-22 18:05:46 +0200

    secure change in admin

--- controllers/admin.js
+++ controllers/admin.js
@@ -94,7 +94,7 @@ exports.postAdminLogin = async (req, res) => {
     // Set cookie (like user/seller login)
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,        
+      secure: true,        
       sameSite: 'none',      
       maxAge: 3 * 60 * 60 * 1000 
     });
